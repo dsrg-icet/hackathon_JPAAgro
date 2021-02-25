c02706cb2e6add1685bc42a64c235d4d3ea86d1e
Erick Maziero <egmaziero@gmail.com>
2021-02-25 12:54:36 -0300

    Updates

--- dataset/evaluate.py
+++ dataset/evaluate.py
@@ -12,7 +12,13 @@ with open(f'{pred_file}', 'r') as input_file:
 y_true = [float(y.strip()) for y in y_true]
 y_pred = [float(y.strip()) for y in y_pred]
 
+truths = []
+preds = []
+for i, true_value in enumerate(y_true):
+    if not np.isnan(true_value):
+        truths.append(true_value)
+        preds.append(y_pred[i])
 
 print(np.sqrt(
-        np.square(
-            np.subtract(y_true, y_pred)).mean()))
+    np.square(
+        np.subtract(truths, preds)).mean()))
